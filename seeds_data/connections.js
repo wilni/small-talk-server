d80acd69a71619d4deba96ee97292580bb5bdee2
Will Tavarez <will@Wills-MBP.localdomain>
2022-08-08 10:19:41 -0400

    edited seeds data removed console logs

--- seeds_data/connections.js
+++ seeds_data/connections.js
@@ -1,32 +1,13 @@
+
 module.exports = [
     {
-        email_1: "wilniyobr.tavarez002@mymdc.net",
-        email_2: "wilni147@aim.com",
-        connection_id: "1",
-    },
-    {
-        email_1: "wilniyobr.tavarez002@mymdc.net",
-        email_2: "john@aim.com",
-        connection_id: "2",
-    },
-    {
-        email_1: "wilniyobr.tavarez002@mymdc.net",
+        email_1: "wilnitavarez@gmail.com",
         email_2: "raquel@aim.com",
-        connection_id: "3",
+        connection_id: '1',
     },
     {
-        email_1: "wilniyobr.tavarez002@mymdc.net",
+        email_1: "wilnitavarez@gmail.com",
         email_2: "rocky@aim.com",
-        connection_id: "4",
-    },
-    {
-        email_1: "wilniyobr.tavarez002@mymdc.net",
-        email_2: "wilnitavarez@gmail.com",
-        connection_id: "5",
-    },
-    {
-        email_1: "raquel@aim.com",
-        email_2: "wilnitavarez@gmail.com",
-        connection_id: "6",
+        connection_id: '2',
     }
 ]
--- seeds_data/messages.js
+++ seeds_data/messages.js
@@ -1,37 +1,19 @@
 module.exports = [
     {
         message_id: "1",
-        sender_email: "wilniyobr.tavarez002@mymdc.net",
-        recipient_email: 'wilni147@aim.com',
+        sender_email: "wilnitavarez@gmail.com",
+        recipient_email: 'raquel@aim.com',
         content: "first text",
         sent_at: Date.now().toString(),
         connection_id: '1'
     },
     {
         message_id: '2',
-        sender_email: "wilni147@aim.com",
-        recipient_email: "wilniyobr.tavarez002@mymdc.net",
-        connection_id: "1",
-        content: "HI, im the personal account", 
+        sender_email: "wilnitavarez@gmail.com",
+        recipient_email: "rocky@aim.com",
+        connection_id: "2",
+        content: "HI!", 
         sent_at: (Date.now() + 1).toString(),
-        connection_id: '1'
-    },
-    {
-        message_id: '3',
-        sender_email: "john@aim.com",
-        recipient_email: "wilniyobr.tavarez002@mymdc.net",
-        connection_id: "1",
-        content: "HI, im the personal account", 
-        sent_at: Date.now().toString(),
-        connection_id: '2'
-    },
-    {
-        message_id: '4',
-        sender_email: "wilniyobr.tavarez002@mymdc.net",
-        recipient_email: "john@aim.com",
-        connection_id: "1",
-        content: "HI, im the personal account", 
-        sent_at: Date.now().toString(),
         connection_id: '2'
     }
     
